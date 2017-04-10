d98a18494517da93ac24def4fbab358e1d0ee37d
Kristopher Parrillo <kkparrillo@gmail.com>
2017-04-10 11:40:22 -0700

    adds link to email and shows time updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,10 @@ class Footer extends Component {
     return(
       <div>
         <Jumbotron style={ {background: 'gray', color: 'white'} }>
+          <h3>Updated 04/10/2017</h3>
           <h3>Valued earnings rates are based on <a href="http://www.thepointsguy.com">ThePointsGuy</a></h3>
           <h5>Created by Kristopher Parchen</h5>
-          <h5>Email: kkparchen@gmail.com</h5>
+          <h5>Contact: <a href="mailto:kkparchen@gmail.com">kkparchen@gmail.com</a></h5>
         </Jumbotron>
       </div>
     )

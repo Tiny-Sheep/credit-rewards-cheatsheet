425c367c82bc88f9ad64e5c1afa0e45bef865a09
Kristopher Parrillo <kkparrillo@gmail.com>
2017-04-12 11:19:41 -0700

    adds first thumbnail to DisplayCard

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { Image } from 'react-bootstrap';
 
 class DisplayCard extends Component {
   render() {
@@ -8,6 +8,7 @@ class DisplayCard extends Component {
     let category = this.props.category
     return(
       <div>
+        <Image src={card.image} thumbnail></Image>
         <h2>Use <span style={ {color: '#004d99', fontSize: '30px'} }>{card.name}</span> For <span style={ {color: '#004d99', fontSize: '30px'} }>{card.category[category]}x</span> Reward Points!</h2>
         <h2>Which is valued at <span style={ {color: 'green', fontSize: '30px'}}> { (card.valuedEarning * card.category[category]).toFixed(1)}</span> cents per dollar spent!</h2>
       </div>

9bb9489037c6bdf45d8e2521f9bf6222a1e48656
Kristopher Parrillo <kkparrillo@gmail.com>
2017-05-25 10:18:24 -0700

    adds textstyle to usercardsdisplay

--- src/components/UserCardsDisplay.js
+++ src/components/UserCardsDisplay.js
@@ -6,9 +6,10 @@ class UserCardsDisplay extends Component {
     
     renderCards() {
         const { userCards } =  this.props;
+
         console.log(userCards)
         if (userCards.length < 1) {
-            return <div>Add Your Cards</div>;
+            return <div></div>;
         }
 
         const cardList = userCards.map(card => <li key={card.name}>{card.name}</li>)
@@ -16,13 +17,23 @@ class UserCardsDisplay extends Component {
     }
     
     render() {
+        const { textStyle } = styles;
+        
         return (
-            <div>
+            <div className="user-cards-display">
+                <h2 style={textStyle}>Your Cards</h2>
                 {this.renderCards()}
             </div>
         );
     }
 }
+
+const styles = {
+    textStyle: {
+        textShadow: '2px 2px 3px black',
+        color: 'white'
+    }
+};
 
 function mapStateToProps(state) {
     return {

f83c3d8ae8c62c90194c58bf277ae4183c622e20
Kristopher Parrillo <kkparrillo@gmail.com>
2017-08-23 16:28:28 -0700

    adds https to image links

--- src/components/GettingStarted.js
+++ src/components/GettingStarted.js
@@ -5,31 +5,31 @@ const GettingStarted = () => (
     <div className="getting-started">
         <Carousel style={styles.carouselStyle} controls={false}>
             <Carousel.Item>
-                <img width={1600} height={500} alt="900x500" src='http://i.imgur.com/v8hLYHu.jpg'/>
+                <img width={1600} height={500} alt="900x500" src='https://i.imgur.com/v8hLYHu.jpg'/>
                 <Carousel.Caption>
                 
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img width={1600} height={500} alt="900x500" src='http://i.imgur.com/PBeVtw7.jpg'/>
+                <img width={1600} height={500} alt="900x500" src='https://i.imgur.com/PBeVtw7.jpg'/>
                 <Carousel.Caption>
 
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img width={1600} height={300} alt="900x500" src='http://i.imgur.com/V7PHVtP.jpg'/>
+                <img width={1600} height={300} alt="900x500" src='https://i.imgur.com/V7PHVtP.jpg'/>
                 <Carousel.Caption>
 
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img width={1600} height={300} alt="900x500" src='http://i.imgur.com/iLOS4bm.jpg'/>
+                <img width={1600} height={300} alt="900x500" src='https://i.imgur.com/iLOS4bm.jpg'/>
                 <Carousel.Caption>
 
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-                <img width={1600} height={300} alt="900x500" src='http://i.imgur.com/lhujmHT.jpg'/>
+                <img width={1600} height={300} alt="900x500" src='https://i.imgur.com/lhujmHT.jpg'/>
                 <Carousel.Caption>
 
                 </Carousel.Caption>
